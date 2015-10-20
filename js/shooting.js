d3f00c45519494ef80225b365f8ec4546d91967a
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-20 14:30:01 -0400

    images and js

---
+++ js/shooting.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  // selector for bowling ball
+  $('#').click(function() {
+    $(this).animate({
+      bottom: "400px"}).animate({left: "370px"}).animate({top: "45px"});
+  });
+
+  // selector for bowling ball
+  $('#').click(function() {
+    $(this).animate({
+      bottom: "400px"}).animate({left: "370px"}).animate({top: "30px"});
+  });
+
+  // selector for beachball
+  $('#').click(function() {
+    $(this).animate({
+      bottom: "420px"}).animate({left: "360px"}).animate({top: "20px"}).animate({left: "400px"});
+  });
+
+  // selector for tennis ball
+  $('#').click(function() {
+    $(this).animate({
+      bottom: "510px"}).animate({left: "450px"}).animate({top: "20px"}).animate({left: "500px"})
+  });
+
+
+
+
+});
