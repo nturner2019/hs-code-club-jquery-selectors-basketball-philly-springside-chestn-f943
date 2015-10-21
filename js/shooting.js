29f0cef50d0148ace8968094ca90a5e8d989b01a
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-21 10:54:33 -0400

    better

--- js/shooting.js
+++ js/shooting.js
@@ -3,22 +3,22 @@ $(document).ready(function(){
   // selector for bowling ball
   $('#bowling-ball').click(function() {
     $(this).animate({
-      bottom: "400px"}).animate({left: "370px"}).animate({top: "45px"});
+      bottom: "400px"}).animate({left: "330px"}).animate({top: "45px"}).animate({left: "420px"});
   });
 
   $('.basketball').click(function() {
     $(this).animate({
-      bottom: "400px"}).animate({left: "370px"}).animate({top: "30px"});
+      bottom: "400px"}).animate({left: "290px"}).animate({top: "30px"});
   });
 
   $('img[alt="rainbow beachball"]').click(function() {
     $(this).animate({
-      bottom: "420px"}).animate({left: "360px"}).animate({top: "20px"}).animate({left: "400px"});
+      bottom: "420px"}).animate({left: "200px"}).animate({top: "20px"}).animate({left: "400px"});
   });
 
   $('div#tennis-ball img').click(function() {
     $(this).animate({
-      bottom: "510px"}).animate({left: "450px"}).animate({top: "20px"}).animate({left: "500px"})
+      bottom: "510px"}).animate({left: "190px"}).animate({top: "20px"}).animate({left: "500px"})
   });
 
 
