0350ee18efb8314e7d9a231bf82ed86bbffc1a00
nturner2019 <nturner@sch.org>
2016-03-02 13:34:47 +0000

    Done.

--- js/ball.js
+++ js/ball.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 
   // selector for bowling ball where you see __ (keep the single quotes!)
-  $('__').click(function() {
+  $('#bowling-ball').click(function() {
     $(this).animate({
       bottom: "400px"}).animate({left: "330px"}).animate({top: "45px"}).animate({left: "420px"});
   });
 
   // Selector for basketball where you see __ (keep the single quotes!)
-  $('__').click(function() {
+  $('.basketball').click(function() {
     $(this).animate({
       bottom: "400px"}).animate({left: "290px"}).animate({top: "30px"});
   });
 
   // Selector for beachball where you see __ (keep the single quotes!)
-  $('__').click(function() {
+  $('img[alt="rainbow beachball').click(function() {
     $(this).animate({
       bottom: "420px"}).animate({left: "200px"}).animate({top: "20px"}).animate({left: "400px"});
   });
 
   // Selector for tennis ball where you see __ (keep the single quotes!)
-  $('__').click(function() {
+  $('#tennis-ball img').click(function() {
     $(this).animate({
       bottom: "510px"}).animate({left: "190px"}).animate({top: "20px"}).animate({left: "500px"})
   });
